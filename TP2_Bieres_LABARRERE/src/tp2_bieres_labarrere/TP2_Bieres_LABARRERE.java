d1351e96c48dc86a7f45074c0e96f3b2a1846cf5
elise <lab.elise@outlook.fr>
2023-09-22 12:30:55 +0200

    appel de la class bouteilleBiere

--- TP2_Bieres_LABARRERE/src/tp2_bieres_labarrere/TP2_Bieres_LABARRERE.java
+++ TP2_Bieres_LABARRERE/src/tp2_bieres_labarrere/TP2_Bieres_LABARRERE.java
@@ -15,6 +15,7 @@ public class TP2_Bieres_LABARRERE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        BouteilleBiere uneBiere = new BouteilleBiere();
     }
-    
+
 }
