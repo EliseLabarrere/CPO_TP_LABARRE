d68f77080c396b1f5be2992e78f4f34ed786fe8f
elise <lab.elise@outlook.fr>
2023-09-22 23:54:19 +0200

    Création convertisseur class TP2 temp

--- TP2_convertisseurObjet_LABARRERE/src/tp2_convertisseurobjet_labarrere/Convertisseur.java
+++ TP2_convertisseurObjet_LABARRERE/src/tp2_convertisseurobjet_labarrere/Convertisseur.java
@@ -9,5 +9,42 @@ package tp2_convertisseurobjet_labarrere;
  * @author Pedro Pablo Palmito
  */
 public class Convertisseur {
+    int nbConversions = 0;
+    String tempFrom = "Celcius";
+    String tempTo = "Celcius";
+
+    public float KelvinVersCelcius(float tempK) {
+        nbConversions++;
+        return tempK - 273.15f;
+    }
+
+    public float CelciusVersKelvin(float tempC) {
+        nbConversions++;
+        return tempC + 273.15f;
+    }
+
+    public float FarenheitVersCelcius(float tempF) {
+        nbConversions++;
+        return (tempF - 32) * (5.0f / 9);
+    }
+
+    public float CelciusVersFarenheit(float tempC) {
+        nbConversions++;
+        return tempC * (9.0f / 5) + 32;
+    }
+
+    public float KelvinVersFarenheit(float tempK) {
+        nbConversions++;
+        return CelciusVersFarenheit(KelvinVersCelcius(tempK));
+    }
+
+    public float FarenheitVersKelvin(float tempF) {
+        nbConversions++;
+        return CelciusVersKelvin(FarenheitVersCelcius(tempF));
+    }
     
+    @Override
+    public String toString() {
+        return "Convertion n°" + nbConversions + "\n";
+    }
 }
